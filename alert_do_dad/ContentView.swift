90dadfcf60983874421d4e2c47a9b063bcd69c47
Jeremy Stein <saplyng@gmail.com>
2020-11-30 15:45:21 -0600

    Update ContentView.swift
    alert practice thing

--- alert_do_dad/ContentView.swift
+++ alert_do_dad/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var warningAlert = false
+    @State var warning2Alert = false
+    @State var selectedDate = Date()
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Button("Warning"){
+                self.warningAlert.toggle()
+            }
+            .alert(isPresented: $warningAlert) { () -> Alert in
+                Alert(title: Text("Warning"), message: Text("Zombies are loose!"))
+            }
+                .padding()
+            
+            Button("Warning 2"){
+                self.warning2Alert.toggle()
+            }
+            .alert(isPresented: $warning2Alert) { () -> Alert in
+                let primaryButton = Alert.Button.default(Text("OK")){print("ok button pressed")}
+                let secondaryButton = Alert.Button.cancel(Text("Cancel")){
+                    print("cancel")
+                }
+                return Alert(title: Text("Warning"), message: Text("Zombies are loose!"), primaryButton: primaryButton, secondaryButton: secondaryButton)
+            }
             .padding()
+            
+            DatePicker("Date for zombie apocalypse", selection: $selectedDate, displayedComponents: .date)
+        }
     }
 }
 
