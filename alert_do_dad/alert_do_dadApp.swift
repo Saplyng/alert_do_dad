de006499220ffe66ac841be47de6a5d4fb55ca19
Jeremy Stein <saplyng@gmail.com>
2020-11-30 05:37:59 -0600

    Initial Commit

---
+++ alert_do_dad/alert_do_dadApp.swift
@@ -0,0 +1,17 @@
+//
+//  alert_do_dadApp.swift
+//  alert_do_dad
+//
+//  Created by student on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct alert_do_dadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
